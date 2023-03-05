187ebc3592fd7b5e411e5426cfc4ab5239e35edc
JAGATHGURU <jagathnanu@gmail.com>
2023-03-05 09:03:43 +0530

    Headers Changed Promo Video Added Team Name Changed In Footer

--- rockfesthtml-10/rockfesthtml-10/Rockfest/Rockfest HTML/js/countdown-custom.js
+++ rockfesthtml-10/rockfesthtml-10/Rockfest/Rockfest HTML/js/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 4, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 02, 23, 0)}); // year, month, date, hour
         });
 });		
 
